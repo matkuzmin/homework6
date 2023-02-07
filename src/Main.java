2c018bb7d35e24740ba9ab69e551174dcacde7ad
matvey <qjlf@inbox.ru>
2023-02-07 16:38:08 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+    public static void task1(){
+        System.out.println("Задача 1");
+for(int i = 1; i<11;i++){
+    System.out.println(i);
+        }
+    }
+
+    public static void task2(){
+        System.out.println("Задача 2");
+        for(int i = 10;i>0;i-=1){
+            System.out.println(i);
+        }
+    }
+    public static void task3(){
+        System.out.println("Задача 3");
+        for(int i = 0;i<17;i+=2){
+            System.out.println(i);
+        }
+    }
+    public static void task4(){
+        System.out.println("Задача 4");
+        for(int i = 0;i<17;i+=2){
+            System.out.println(i);
+        }
     }
 }
